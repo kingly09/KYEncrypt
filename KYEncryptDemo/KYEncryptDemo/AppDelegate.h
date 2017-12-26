711abfb7f369ca073536f0653c2cc806b0c9fac5
kingly <libintm@163.com>
2017-12-26 10:34:03 +0800

    初始化

---
+++ KYEncryptDemo/KYEncryptDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KYEncryptDemo
+//
+//  Created by kingly on 2017/12/26.
+//  Copyright © 2017年 kingly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KYEncryptDemo/KYEncryptDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KYEncryptDemo
+//
+//  Created by kingly on 2017/12/26.
+//  Copyright © 2017年 kingly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
